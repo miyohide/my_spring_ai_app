44fa7b3ae94145b997335ee00dd2d0efd98617fb
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-21 22:58:16 +0900

    add logger

--- src/main/java/com/github/miyohide/demo/DemoApplication.java
+++ src/main/java/com/github/miyohide/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
+@Slf4j
 public class DemoApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
@@ -14,6 +16,6 @@ public class DemoApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("Hello World!");
+    log.info("Hello World!");
   }
 }

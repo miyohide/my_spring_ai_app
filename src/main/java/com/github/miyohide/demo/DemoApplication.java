173c83a4e891608d60dd13096a5a5fecc71155a6
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-21 22:53:23 +0900

    run format

--- src/main/java/com/github/miyohide/demo/DemoApplication.java
+++ src/main/java/com/github/miyohide/demo/DemoApplication.java
@@ -8,15 +8,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(DemoApplication.class)
-		.web(WebApplicationType.NONE)
-		.run(args);
-	}
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(DemoApplication.class).web(WebApplicationType.NONE).run(args);
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
-	}
-
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Hello World!");
+  }
 }

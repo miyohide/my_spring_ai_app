94c777be76edb5bc156db09fc8ecc76a0c157555
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-21 22:44:27 +0900

    コマンドラインアプリで動くようにした

--- src/main/java/com/github/miyohide/demo/DemoApplication.java
+++ src/main/java/com/github/miyohide/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.github.miyohide.demo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		new SpringApplicationBuilder(DemoApplication.class)
+		.web(WebApplicationType.NONE)
+		.run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
 	}
 
 }

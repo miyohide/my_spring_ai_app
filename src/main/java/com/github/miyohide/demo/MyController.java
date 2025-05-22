35b4ec8fb3358a285c76a28d5e34e1e0278ec1e5
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-22 13:14:04 +0900

    run spotless

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -1,31 +1,24 @@
 package com.github.miyohide.demo;
 
 import java.util.Map;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class MyController {
-    private final ChatModel chatModel;
+  private final ChatModel chatModel;
 
-    public MyController(ChatModel chatModel) {
-        this.chatModel = chatModel;
-    }
+  public MyController(ChatModel chatModel) {
+    this.chatModel = chatModel;
+  }
 
-    @GetMapping("/ai/gen")
-    public Map<String, String> getWeather() {
-        String response = ChatClient.create(this.chatModel)
-        .prompt("東京の天気は？")
-        .call()
-        .content();
-        return Map.of("gen", response);
-    }
-
+  @GetMapping("/ai/gen")
+  public Map<String, String> getWeather() {
+    String response = ChatClient.create(this.chatModel).prompt("東京の天気は？").call().content();
+    return Map.of("gen", response);
+  }
 }

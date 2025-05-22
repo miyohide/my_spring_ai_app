4b411bb7c81e02b26d746b0ea92c8838826d9329
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-22 21:40:18 +0900

    use stream

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -2,9 +2,12 @@ package com.github.miyohide.demo;
 
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @Slf4j
@@ -19,5 +22,10 @@ public class MyController {
   public Map<String, String> getWeather() {
     String response = this.chatClient.prompt("東京の天気は？").call().content();
     return Map.of("gen", response);
+  }
+
+  @GetMapping("/ai/genstream")
+  public Flux<String> getWeatherStream() {
+    return this.chatClient.prompt("東京の天気は？").stream().content();
   }
 }

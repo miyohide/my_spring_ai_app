22b44ddc429d142bf8f39c3e39ca0e9c42e5b615
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-22 21:36:56 +0900

    use chatClient

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -3,22 +3,21 @@ package com.github.miyohide.demo;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class MyController {
-  private final ChatModel chatModel;
+  private final ChatClient chatClient;
 
-  public MyController(ChatModel chatModel) {
-    this.chatModel = chatModel;
+  public MyController(ChatClient.Builder builder) {
+    this.chatClient = builder.build();
   }
 
   @GetMapping("/ai/gen")
   public Map<String, String> getWeather() {
-    String response = ChatClient.create(this.chatModel).prompt("東京の天気は？").call().content();
+    String response = this.chatClient.prompt("東京の天気は？").call().content();
     return Map.of("gen", response);
   }
 }

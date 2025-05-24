c43bc2508d2e87806ebf5832261dcda386c16dde
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-24 14:47:07 +0900

    add crossorigin

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -6,6 +6,7 @@ import reactor.core.publisher.Flux;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin
 @Slf4j
 public class MyController {
   private final ChatClient chatClient;

f9dbd2d01fc1bf2efd961412843f9955ba07f961
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-24 10:41:26 +0900

    delete unused import statement

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;

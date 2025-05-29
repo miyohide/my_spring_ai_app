cc1e08b3e468f319f8b6e8fc67d2cd3510994cb6
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-29 21:41:22 +0900

    add chatmemory

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -2,14 +2,15 @@ package com.github.miyohide.demo;
 
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
-
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import reactor.core.publisher.Flux;
 
 @RestController
 @Slf4j
@@ -17,21 +18,21 @@ public class MyController {
   private final ChatClient chatClient;
 
   public MyController(ChatClient.Builder builder) {
-    this.chatClient = builder.build();
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+    this.chatClient =
+        builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
   }
 
   @PostMapping("/chat")
   public Map<String, String> chat(
-    @RequestParam(value = "message", defaultValue = "Tell me a joke") String message
-  ) {
+      @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
     String response = this.chatClient.prompt(message).call().content();
     return Map.of("response", response);
   }
 
   @GetMapping("/chatstream")
   public Flux<String> chatStream(
-    @RequestParam(value = "message", defaultValue = "Tell me a joke") String message
-  ) {
+      @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
     return this.chatClient.prompt(message).stream().content();
   }
 }

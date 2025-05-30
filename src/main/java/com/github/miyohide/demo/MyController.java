45151bb09ed9d088597040b0ba2502458ccfc7a9
Hidenori Miyoshi <miyohide@gmail.com>
2025-05-30 21:42:24 +0900

    use chatMemoryRepository

--- src/main/java/com/github/miyohide/demo/MyController.java
+++ src/main/java/com/github/miyohide/demo/MyController.java
@@ -1,12 +1,15 @@
 package com.github.miyohide.demo;
 
+import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +19,13 @@ import reactor.core.publisher.Flux;
 @Slf4j
 public class MyController {
   private final ChatClient chatClient;
+  private final ChatMemoryRepository chatMemoryRepository;
 
-  public MyController(ChatClient.Builder builder) {
+  public MyController(ChatClient.Builder builder, ChatMemoryRepository chatMemoryRepository) {
     ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
     this.chatClient =
         builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
+    this.chatMemoryRepository = chatMemoryRepository;
   }
 
   @PostMapping("/chat")
@@ -34,5 +39,15 @@ public class MyController {
   public Flux<String> chatStream(
       @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
     return this.chatClient.prompt(message).stream().content();
+  }
+
+  @GetMapping("/chathistories")
+  public List<String> getChatHistories() {
+      return chatMemoryRepository.findConversationIds();
+  }
+
+  @GetMapping("/chathistories/{id}")
+  public String getChatHitory(@PathVariable("id") String id) {
+      return chatMemoryRepository.findByConversationId(id).getFirst().toString();
   }
 }
